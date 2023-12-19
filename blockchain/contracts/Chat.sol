49e953a2683ebad97e3be7f60ec943aea2dfea39
Nicolas Vycas Nery <nicolas@ditavia.com>
2023-12-18 21:22:50 -0300

    Fix pagination bug in get function

--- blockchain/contracts/Chat.sol
+++ blockchain/contracts/Chat.sol
@@ -12,7 +12,7 @@ contract Chat {
     }
 
     event MessageSent(string text, address sender, uint timestamp);
-    
+
     Message[] public messages;
 
     function send(string memory _text) public {
@@ -24,21 +24,22 @@ contract Chat {
         return messages.length;
     }
 
-    function get(uint page, uint perPage) public view returns (Message[] memory) {
-        uint startIndex = page * perPage;
-        uint endIndex = (page + 1) * perPage;
-
-        if (endIndex > messages.length) {
-            endIndex = messages.length;
+    function get(
+        uint256 _page,
+        uint256 _perPage
+    ) public view returns (Message[] memory) {
+        if (_page == 0 || _perPage == 0) {
+            return new Message[](0); // return empty array
         }
-
-        Message[] memory result = new Message[](endIndex - startIndex);
-
-        for (uint i = startIndex; i < endIndex; i++) {
-            result[i - startIndex] = messages[i];
+        uint256 start = (_page - 1) * _perPage;
+        uint256 end = start + _perPage;
+        if (end > messages.length) {
+            end = messages.length;
         }
-
+        Message[] memory result = new Message[](end - start);
+        for (uint256 i = start; i < end; i++) {
+            result[i - start] = messages[i];
+        }
         return result;
     }
-    
 }
